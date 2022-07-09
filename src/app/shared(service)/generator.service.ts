8981c6363b2b6616275b4c4923507221cb45bc53
Evgeniy Araslanov <araslanov21@gmail.com>
2022-07-10 02:26:44 +0400

    pisya popa

--- src/app/shared(service)/generator.service.ts
+++ src/app/shared(service)/generator.service.ts
@@ -4,8 +4,11 @@ import {DataService} from "./data.service";
 @Injectable({
   providedIn: 'root'
 })
+
 export class GeneratorService {
 
   constructor(private dataService: DataService) {
   }
 }
+
+
