9339a195dbb8d79f68b88a8010c7223f65cbc617
Evgeniy Araslanov <araslanov21@gmail.com>
2022-07-21 02:22:04 +0400

    поправил сохранение в csv, теперь там есть столбцы, в json тоже можно сохранять, кнопочки сделал более менее красивыми

--- src/app/shared(service)/save-load.service.ts
+++ src/app/shared(service)/save-load.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import {DataService} from "./data.service";
-
+import { saveAs } from 'file-saver';
 @Injectable({
   providedIn: 'root'
 })
 export class SaveLoadService {
-
 
   load(data:any){
     this.dataService.ELEMENT_DATA = JSON.parse(data.content)
@@ -22,19 +21,27 @@ export class SaveLoadService {
     link.remove();
   }
 
-  saveCsv(dat:any){
-    let data1:string = JSON.stringify(dat)
-    let fileName:string = "dat" + '.csv'
-    const file = new Blob([data1], { type: "text/plain" })
+  saveCsv(data: any) {
+    const replacer = (key: any, value: null) => value === null ? '' : value;
+    const header = Object.keys(data[0]);
+    let csv = data.map((row: { [x: string]: any; }) => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+    csv.unshift(header.join(','));
+    let csvArray = csv.join('\r\n');
+
+    var blob = new Blob([csvArray], {type: 'text/csv' })
+    saveAs(blob, "csv.csv");
+  }
+
+  saveJson(dat: any) {
+    let JSON1 = JSON.stringify(dat);
+    let fileName:string = "json" + '.json'
+    let blob = new Blob([JSON1], { type: 'text/json' });
+    let url= window.URL.createObjectURL(blob);
     const link = document.createElement("a");
-    link.href = URL.createObjectURL(file);
+    link.href = URL.createObjectURL(blob);
     link.download = fileName;
     link.click();
     link.remove();
-  }
-
-  saveJson(dat:any){
-    console.log('In progress')
   }
 
   constructor(private dataService: DataService) {
